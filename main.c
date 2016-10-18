b10089cb2c6369ddcb42f9a53dd15ad760cc5098
Sergey Melentyev <sergey.melentyev@gmail.com>
2016-10-18 18:44:09 +0400

    Update main.c

--- main.c
+++ main.c
@@ -1,17 +1,58 @@
-#define NAME replacement_text		// symbolic name or symbolic constant
-''		// character constant
-""		// character string or string constant
-
-int extern_var_name;		// external variable definition
-extern int extern_var_name;		// way to access it from function scope
+#define NAME replacement_text		// symbolic constant
+enum boolean { NO, YES };		// enumeration constant
+char name = '';		// character constant
+char array[] = "";		// string constant
 
 
+// QUALIFIERS AND MODIFICATORS
+auto int x;		// in block auto memory manager, local scope, init with random value
+const double e = 2.7182818;		//constant qualifier
+void func_name(const char[]);		// constatnt qualifier as an array argument
+int extern_var_name;		// external variable definition, global file scope, init with 0
+extern int extern_var;		// the way to access external variable from func / file scope
+static int static_var;		// in block memory manager, local block/file scope, init with 0
+register int var_name;		// only in block cpu register memory, no pointers, init with random
+int number = (int) e;		// explicit type conversion
+int func_wrap(char s[]) { return (int) func_that_return_dbl(s); }		// explicit return type conversion
+
+
+// CONTROL FLOW
+int conditional_expr = (val_one > val_two) ? val_one : val_two;
+for (i = 0, j = strlen(s) - 1; i < j; i++, j--)
+	c = s[i], s[i] = s[j], s[j] = c;
+switch (expression) {
+	case const_expr: statement;
+		break;
+	default: statement;
+}
+while (expression) {
+	statements;
+}
+do {
+	statements;
+} while (expression);
+
+
+// ARRAY
+int days[] = {1, 2, 3, 4};		// initialized array
+char patter[] = "name";		// the same as char patter[]= {'n', 'a', 'm', 'e', '\0'};
+
+
+// RECURSION
+void recursionCall(int n) {
+    if (n < 5)
+        recursionCall(n + 1);
+}
+
+void recursionTailCall(int n) {
+    if (n < 5)
+        recursionTailCall(n + 1);
+    return;
+}
 
 
 
-auto int x;   // in block auto memory manager, local scope, init with random val
-register int y;   // in block cpu register memory, no pointers allow, local scope, init with random val
-static int z;   // in block memory manager, local scope, init with 0, save value while program runs
+
 volatile int a;     // value could be changed while program runs, do not optimize (compiler)
 restrict int *b;     // pointer could be optimized (compiler), if the pointer is unique
 
@@ -30,13 +71,29 @@ ar = (int (*)[column])malloc(row * column * sizeof(int));    // array row * colu
 free(ar);
 
 
-void recursionCall(int n) {
-    if (n < 5)
-        recursionCall(n + 1);
+// ALGORITHMS
+int binary_search(int value, int arr_body[], int arr_length) {
+	int low, high, mid;
+	low = 0;
+	high = arr_length - 1;
+	while (low <= high) {
+		mid = (low + high) / 2;
+		if (value < arr_body[mid])
+			high = mid - 1;
+		else if (value > arr_body[mid])
+			low = mid + 1;
+		else
+			return mid;
+	}
+	return -1;
 }
-
-void recursionTailCall(int n) {
-    if (n < 5)
-        recursionTailCall(n + 1);
-    return;
+void shell_sort(int arr_body[], int arr_length) {
+	int gap, i, j, temp;
+	for (gap = arr_length / 2; gap > 0; gap /= 2)
+		for (i = gap; i < arr_length; i++)
+			for (j = i - gap; j >= 0 && arr_body[j] > arr_body[j + gap]; j -= gap) {
+				temp = arr_body[j];
+				arr_body[j] = arr_body[j + gap];
+				arr_body[j + gap] = temp;
+			}
 }
